f9ee0aae9045e54b51fb3d105d1c2b93d9e5bb57
刘豪强 <122520213@qq.com>
2020-10-20 14:25:29 +0800

    后台管理

---
+++ src/router.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+const Login = () =>
+    import ('./components/Login.vue')
+const Home = () =>
+    import ('./components/Home.vue')
+const Welcome = () =>
+    import ('./components/Welcome.vue')
+const Users = () =>
+    import ('./components/user/Users.vue')
+const Rights = () =>
+    import ('./components/power/Rights.vue')
+const Roles = () =>
+    import ('./components/power/Roles.vue')
+
+
+const Cate = () =>
+    import ('./components/goods/Cate(1).vue')
+const Params = () =>
+    import ('./components/goods/Params.vue')
+const GoodsList = () =>
+    import ('./components/goods/List.vue')
+const Add = () =>
+    import ('./components/goods/Add.vue')
+const Order = () =>
+    import ('./components/order/Order.vue')
+const Report = () =>
+    import ('./components/report/Report.vue')
+
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [
+                { path: '/welcome', component: Welcome },
+                { path: '/users', component: Users },
+                { path: '/rights', component: Rights },
+                { path: '/roles', component: Roles },
+                { path: '/categories', component: Cate },
+                { path: '/params', component: Params },
+                { path: '/goods', component: GoodsList },
+                { path: '/goods/add', component: Add },
+                { path: '/orders', component: Order },
+                { path: '/reports', component: Report }
+            ]
+        }
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next()
+        // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+
+export default router
